711b386ff55c5cce2cd9a5388b914f96fe085855
vlad99902 <legkovv@mail.ru>
2020-12-03 23:47:27 +0300

    first version of server

---
+++ models/Debt.js
@@ -0,0 +1,11 @@
+const { Schema, model, Types } = require('mongoose');
+
+const schema = new Schema({
+  title: { type: String, required: true },
+  sum: { type: Number, required: true },
+  completed: { type: Boolean, required: true, default: true },
+  owe: { type: Boolean, required: true, default: true },
+  owner: { type: Types.ObjectId, ref: 'User' },
+});
+
+module.exports = model('Debt', schema);
---
+++ routes/debt.routes.js
@@ -0,0 +1,37 @@
+const { Router, response, request } = require('express');
+const config = require('config');
+const Debt = require('../models/Debt');
+const auth = require('../middleware/auth.middleware');
+const router = Router();
+
+router.post('/add', auth, async (req, res) => {
+  try {
+    const baseUrl = config.get('baseUrl');
+    //TODO логика добавления в бд
+    const { title, sum, completed, owe } = req.body;
+    const debt = new Debt({
+      title,
+      sum,
+      completed,
+      owe,
+      owner: req.user.userId,
+    });
+
+    await debt.save();
+    res.status(201).json(debt);
+  } catch (e) {
+    res.status(500).json({ message: 'Something wrong with add new deb' });
+  }
+});
+
+router.get('/', auth, async (req, res) => {
+  try {
+    //все дебтсы текущего пользователся
+    const debts = await Debt.find({ owner: req.user.userId });
+    res.json(debts);
+  } catch (e) {
+    res.status(500).json({ message: 'Something wrong with get all debts' });
+  }
+});
+
+module.exports = router;
